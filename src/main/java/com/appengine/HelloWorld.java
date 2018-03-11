df5236c09aa1fcecc8367093154e7dfb1fc8f6c8
home <home@HOME-PC>
2018-03-11 17:52:27 +0800

    test

--- src/main/java/com/appengine/HelloWorld.java
+++ src/main/java/com/appengine/HelloWorld.java
@@ -2,13 +2,15 @@ package com.appengine;
  
 import java.util.logging.Logger;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.appengine.model.Webhook;
 import com.appengine.utils.JSONTool;
  
-@Controller
+@RestController
 public class HelloWorld {
 	
 	private static final Logger log = Logger.getLogger(HelloWorld.class.getName());
